b0d67aa9176ba36be58526a597afd7425e93a83a
lyw <1850991069@qq.com>
2020-10-23 21:08:11 +0800

    自己第二次提交4

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <h2>预祝大家前程似锦</h2>
     <img src="https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=324237248,1675971079&fm=26&gp=0.jpg"/>
     <img src="https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=2289170922,101725040&fm=26&gp=0.jpg"/>
-    <img src="https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=324237248,1675971079&fm=26&gp=0.jpg"/>
+    <img src="https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=1467200407,2039930783&fm=26&gp=0.jpg"/>
 
   </div>
 </template>
