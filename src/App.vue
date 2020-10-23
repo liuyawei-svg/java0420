80512ff7860916d95f01f2c20de402432c4dfce8
lyw <1850991069@qq.com>
2020-10-23 21:04:34 +0800

    自己第二次提交3

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <template>
   <div id="app">
     <h2>预祝大家前程似锦</h2>
-    <img src="https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=1243329449,21263175&fm=26&gp=0.jpg"/>
+    <img src="https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=324237248,1675971079&fm=26&gp=0.jpg"/>
     <img src="https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=2289170922,101725040&fm=26&gp=0.jpg"/>
+    <img src="https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=324237248,1675971079&fm=26&gp=0.jpg"/>
+
   </div>
 </template>
 
